76967cda4ab7b5a9a62a966692fa0e82846b288e
Teke Benjamin <benjamin.teke@gmail.com>
2020-02-22 23:59:05 +0100

    Add hand recognition and picture capture functionality

--- client/js/facedetect.js
+++ client/js/facedetect.js
@@ -1,4 +1,11 @@
+const MODEL_URL = '/js/lib/model_web/'
+const LABELS_URL = MODEL_URL + 'labels.json'
+const MODEL_JSON = MODEL_URL + 'model.json'
+
 const video = document.getElementById('video');
+
+let photoCounter = 0;
+let faceDetected = false;
 
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/js/lib/models'),
@@ -19,10 +26,57 @@ video.addEventListener('play', () => {
     faceapi.matchDimensions(canvas, displaySize);
     setInterval(async () => {
         const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        faceDetected = detections.length > 0;
         const resizedDetections = faceapi.resizeResults(detections, displaySize);
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
         faceapi.draw.drawDetections(canvas, resizedDetections);
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-    }, 100)
+    }, 100);
+
+    startTF();
 })
+
+async function startTF() {
+    const modelPromise = tf.loadGraphModel(MODEL_JSON)
+    const labelsPromise = fetch(LABELS_URL).then(data => data.json())
+    const values = await Promise.all([modelPromise, labelsPromise]);
+    [model] = values;
+    detectFrame(video, model);
+}
+  
+async function detectFrame(video, model) {
+    thumbs = await TFWrapper(model).detect(video);
+    const scoreHighEnough = element => element.score > 0.95;
+    if (faceDetected && thumbs.some(scoreHighEnough)) {
+        photoCounter++;
+    } else {
+        photoCounter = 0;
+    }
+
+    if (photoCounter == 5) {
+        console.log("Photo created.");
+        photoCounter = -5;
+        capture();
+    }
+    detectFrame(video, model);
+}
+
+function capture() {
+    let canvas = document.getElementById('picture');     
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0, video.videoWidth, video.videoHeight);  
+    canvas.toBlob(blob => {
+      const img = new Image();
+      let url = window.URL ? URL : webkitURL;
+      img.src = url.createObjectUrl(blob);
+    });
+}
+
+document.getElementById('shareBtn').onclick = function() {
+    FB.ui({
+      display: 'popup',
+      method: 'share',
+      href: 'http://localhost:3000',
+    }, function(response){});
+  }
