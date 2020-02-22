21ae75d6e412443fe180015f0cb3b3a666bbb1eb
Teke Benjamin <benjamin.teke@gmail.com>
2020-02-22 18:28:12 +0100

    Face detection v1.

---
+++ client/js/facedetect.js
@@ -0,0 +1,28 @@
+const video = document.getElementById('video');
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/js/lib/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/js/lib/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/js/lib/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/js/lib/models')
+]).then(startVideo);
+
+function startVideo() {
+    navigator.mediaDevices.getUserMedia({video: {}})
+    .then((stream)=> {video.srcObject = stream;}, (err)=> console.error(err));
+  }
+
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.getElementById('live-view').append(canvas);
+    const displaySize = { width: video.width, height: video.height };
+    faceapi.matchDimensions(canvas, displaySize);
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    }, 100)
+})
