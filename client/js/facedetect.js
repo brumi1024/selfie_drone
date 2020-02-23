15826c27bcff96ed02f62b4c6c65ae981a1595b9
Teke Benjamin <benjamin.teke@gmail.com>
2020-02-23 22:19:16 +0100

    Base functionality finished.

--- client/js/facedetect.js
+++ client/js/facedetect.js
@@ -2,10 +2,18 @@ const MODEL_URL = '/js/lib/model_web/'
 const LABELS_URL = MODEL_URL + 'labels.json'
 const MODEL_JSON = MODEL_URL + 'model.json'
 
+let consentGiven = confirm("Photos will be created of you, do you accept it?");
+
 const video = document.getElementById('video');
+const liveViewText = document.getElementById('liveViewText');
+const photoWillBeCreatedText = document.getElementById('photoWillBeCreatedText');
+const photoCreatedText = document.getElementById('photoCreatedText');
+const downloadBtn = document.getElementById('downloadBtn');
 
 let photoCounter = 0;
 let faceDetected = false;
+
+setStatusText(1);
 
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/js/lib/models'),
@@ -17,24 +25,7 @@ Promise.all([
 function startVideo() {
     navigator.mediaDevices.getUserMedia({video: {}})
     .then((stream)=> {video.srcObject = stream;}, (err)=> console.error(err));
-  }
-
-video.addEventListener('play', () => {
-    const canvas = faceapi.createCanvasFromMedia(video);
-    document.getElementById('live-view').append(canvas);
-    const displaySize = { width: video.width, height: video.height };
-    faceapi.matchDimensions(canvas, displaySize);
-    setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-        faceDetected = detections.length > 0;
-        const resizedDetections = faceapi.resizeResults(detections, displaySize);
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-        faceapi.draw.drawDetections(canvas, resizedDetections);
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-    }, 100);
-
-    startTF();
-})
+}
 
 async function startTF() {
     const modelPromise = tf.loadGraphModel(MODEL_JSON)
@@ -49,12 +40,16 @@ async function detectFrame(video, model) {
     const scoreHighEnough = element => element.score > 0.95;
     if (faceDetected && thumbs.some(scoreHighEnough)) {
         photoCounter++;
+        if (photoCounter >= 0) {
+            setStatusText(2);
+        }
     } else {
+        setStatusText(1);
         photoCounter = 0;
     }
 
     if (photoCounter == 5) {
-        console.log("Photo created.");
+        setStatusText(3);
         photoCounter = -5;
         capture();
     }
@@ -62,21 +57,53 @@ async function detectFrame(video, model) {
 }
 
 function capture() {
-    let canvas = document.getElementById('picture');     
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    canvas.getContext('2d').drawImage(video, 0, 0, video.videoWidth, video.videoHeight);  
-    canvas.toBlob(blob => {
-      const img = new Image();
-      let url = window.URL ? URL : webkitURL;
-      img.src = url.createObjectUrl(blob);
+    let pictureCanvas = document.getElementById('picture');     
+    pictureCanvas.width = video.videoWidth;
+    pictureCanvas.height = video.videoHeight;
+    pictureCanvas.getContext('2d').drawImage(video, 0, 0, video.videoWidth, video.videoHeight);  
+    const img = new Image();
+    let dataUrl = pictureCanvas.toDataURL("image/png");
+    img.src = dataUrl;
+    downloadBtn.setAttribute('download', 'photo.png');
+    downloadBtn.setAttribute('href', dataUrl.replace("image/png", "image/octet-stream"));
+}
+
+function setStatusText(status) {
+    switch(status) {
+        case 1:
+            liveViewText.style.display = 'inline';
+            photoWillBeCreatedText.style.display = 'none';
+            photoCreatedText.style.display = 'none';
+            break;
+        case 2:
+            liveViewText.style.display = 'none';
+            photoWillBeCreatedText.style.display = 'inline';
+            photoCreatedText.style.display = 'none';
+            break;
+        case 3:
+            liveViewText.style.display = 'none';
+            photoWillBeCreatedText.style.display = 'none';
+            photoCreatedText.style.display = 'inline';
+            break;
+    }
+}
+
+if (consentGiven) {
+    video.addEventListener('play', () => {
+        const canvas = faceapi.createCanvasFromMedia(video);
+        document.getElementById('live-view').append(canvas);
+        const displaySize = { width: video.width, height: video.height };
+        faceapi.matchDimensions(canvas, displaySize);
+        setInterval(async () => {
+            const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+            faceDetected = detections.length > 0;
+            const resizedDetections = faceapi.resizeResults(detections, displaySize);
+            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+            faceapi.draw.drawDetections(canvas, resizedDetections);
+            faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+        }, 100);
+
+        startTF();
     });
 }
 
-document.getElementById('shareBtn').onclick = function() {
-    FB.ui({
-      display: 'popup',
-      method: 'share',
-      href: 'http://localhost:3000',
-    }, function(response){});
-  }
